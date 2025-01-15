e863c37425c7017e412346924c47ff088bfb4088
shusuke919 <sh091965@gmail.com>
2025-01-15 15:46:35 +0900

    serverActions

--- components/component/PostForm.tsx
+++ components/component/PostForm.tsx
@@ -5,22 +5,29 @@ import { Button } from "@/components/ui/button";
 import { SendIcon } from "./Icons";
 
 export default function PostForm() {
+  async function addPostAction(formData: FormData) {
+    "use server";
+    const postText = formData.get("post");
+    console.log(postText);
+  }
   return (
     <div className="flex items-center gap-4">
       <Avatar className="w-10 h-10">
         <AvatarImage src="/placeholder-user.jpg" />
         <AvatarFallback>AC</AvatarFallback>
       </Avatar>
-      <Input
-        type="text"
-        placeholder="What's on your mind?"
-        className="flex-1 rounded-full bg-muted px-4 py-2"
-        name="post"
-      />
-      <Button variant="ghost" size="icon">
-        <SendIcon className="h-5 w-5 text-muted-foreground" />
-        <span className="sr-only">Tweet</span>
-      </Button>
+      <form className="flex items-center flex-1" action={addPostAction}>
+        <Input
+          type="text"
+          placeholder="What's on your mind?"
+          className="flex-1 rounded-full bg-muted px-4 py-2"
+          name="post"
+        />
+        <Button variant="ghost" size="icon">
+          <SendIcon className="h-5 w-5 text-muted-foreground" />
+          <span className="sr-only">Tweet</span>
+        </Button>
+      </form>
     </div>
   );
 }
